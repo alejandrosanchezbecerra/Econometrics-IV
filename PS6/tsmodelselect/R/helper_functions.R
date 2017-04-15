9a0d75309dbba9a73792a5d0526ef4ab34a49016
alejandrosanchezbecerra <alesan@sas.upenn.edu>
2017-04-15 16:25:08 -0400

    Update package to include cross validation

--- PS6/tsmodelselect/R/helper_functions.R
+++ PS6/tsmodelselect/R/helper_functions.R
@@ -12,24 +12,24 @@
 #' @export
 #'
 #' @examples
-sample_dataAR <- function(Phi0,Sigma0,p,m,n,...) {
+sample_dataAR <- function(Phi0,Sigma0,p0,m,n,...) {
 
-  epsilon <- MASS::mvrnorm(n+p,mu=rep(0,m),Sigma=Sigma0)
-  data    <- matrix(0,n+p,m)
+  epsilon <- MASS::mvrnorm(n+p0,mu=rep(0,m),Sigma=Sigma0)
+  data    <- matrix(0,n+p0,m)
 
-  for(t in (p+1):(p+n)) {
+  for(t in (p0+1):(p0+n)) {
 
     data[t,] <- epsilon[t,]
-    order    <- 1
 
-    while(order <= p) {
+    order    <- 1
+    while(order <= p0) {
       data[t,] <- data[t,]+matrix(c(data[t-order,]),1,m)%*%t(Phi0[[order]])
       order    <- order+1
     }
 
   }
 
-  return(data[(p+1):(p+n),])
+  return(data[(p0+1):(p0+n),])
 
 }
 
@@ -46,27 +46,163 @@ sample_dataAR <- function(Phi0,Sigma0,p,m,n,...) {
 #' @export
 #'
 #' @examples
-sample_dataMA <- function(Theta0,Sigma0,q,m,n,...) {
+sample_dataMA <- function(Theta0,Sigma0,q0,m,n,...) {
 
-  epsilon <- MASS::mvrnorm(n+q,mu=rep(0,m),Sigma=Sigma0)
-  data    <- matrix(0,n+q,m)
+  epsilon <- MASS::mvrnorm(n+q0,mu=rep(0,m),Sigma=Sigma0)
+  data    <- matrix(0,n+q0,m)
 
-  for(t in (q+1):(n+q)) {
+  for(t in (q0+1):(n+q)) {
 
     data[t,] <- epsilon[t,]
     order    <- 1
 
-    while(order <= q) {
+    while(order <= q0) {
       data[t,] <- data[t,]+matrix(c(epsilon[t-order,]),1,m)%*%t(Theta0[[order]])
       order    <- order+1
     }
 
   }
 
-  return(data[(q+1):(n+q),])
+  return(data[(q0+1):(n+q0),])
 
 }
 
-## TODO: Function that computes MSE given prediction and data.
 
-## TODO: Function that computes criteria
+#' Sample Data from an ARMA model
+#'
+#' @param Theta0
+#' @param Sigma0
+#' @param q
+#' @param m
+#' @param n
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sample_dataARMA <- function(Phi0,Theta0,Sigma0,p0,q0,m,n,...) {
+
+  epsilon <- MASS::mvrnorm(n+max(p0,q0),mu=rep(0,m),Sigma=Sigma0)
+  data    <- matrix(0,n+max(p0,q0),m)
+  data[1:max(p0,q0),] <- epsilon[1:max(p0,q0),]
+
+  for(t in (max(p0,q0)+1):(n+max(p0,q0))) {
+
+    data[t,] <- epsilon[t,]
+
+    order    <- 1
+    while(order <= q0 & (q0 != 0)) {
+      data[t,]  <- data[t,]+matrix(c(epsilon[t-order,]),1,m)%*%t(Theta0[[order]])
+      order     <- order + 1
+    }
+
+    order    <- 1
+    while(order <= p0 & (p0 != 0)) {
+
+      data[t,]  <- data[t,]+matrix(c(data[t-order,]),1,m)%*%t(Phi0[[order]])
+      order     <- order +1
+    }
+
+  }
+
+  return(data[(max(p0,q0)+1):(n+max(p0,q0)),])
+
+}
+
+#' Title
+#'
+#' @param X
+#' @param Y
+#' @param h
+#' @param v
+#' @param t
+#'
+#' @return
+#' @export
+#'
+#' @examples
+construct_training <- function(X,Y,h,v,t) {
+
+  training_data <- list()
+
+  if(t <= h+v) {
+    training_data$X <- X[(h+v+t+1):length(Y),]
+    training_data$Y <- Y[(h+v+t+1):length(Y),]
+  } else if (t > h+v & t < dim(Y)[1]-h-v ) {
+    training_data$X <- X[-((t-h-v):(t+h+v)),]
+    training_data$Y <- Y[-((t-h-v):(t+h+v)),]
+  } else {
+    training_data$X <- X[1:(t-h-v-1),]
+    training_data$Y <- Y[1:(t-h-v-1),]
+  }
+
+  return(training_data)
+
+}
+
+construct_test   <- function(X,Y,h,v,t) {
+
+  test_data     <- list()
+  test_data$X   <- X[(t-v):(t+v),]
+  test_data$Y   <- Y[(t-v):(t+v),]
+
+  return(test_data)
+}
+
+outofsample_fit <- function(X,Y,h,v,t,p) {
+
+  # print(t)
+
+  training_data <- construct_training(X,Y,h,v,t)
+  test_data     <- construct_test(X,Y,h,v,t)
+
+  beta_train_model   <- do.call(function(X,Y)
+                                solve(t(as.matrix(X))%*%as.matrix(X),
+                                      t(as.matrix(X))%*%as.matrix(Y)),
+                                training_data)
+
+  pred_error         <- test_data$Y - as.matrix(test_data$X) %*% beta_train_model
+
+  # train_model   <- arima(training_data,order=c(p,0,0),include.mean=FALSE)
+  # test_model    <- arima(training_data,order=c(p,0,0),include.mean=FALSE,
+  #                       fixed = train_model$coef)
+
+  MSE_t         <- t(pred_error)%*%(pred_error)/(length(test_data))
+
+  return(MSE_t)
+
+}
+
+
+#' Title
+#'
+#' @param data
+#' @param h
+#' @param v
+#' @param p
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cv_AR <- function(data,h,v,p) {
+
+  Y <- data[(p+1):length(data)]
+  X <- matrix(NA,length(data)-p,0)
+
+  for(lag in 1:p) {
+     X <- cbind(X,data[(p+1-lag):(length(data)-lag)])
+  }
+
+  Y <- as.data.frame(Y)
+  X <- as.data.frame(X)
+
+  MSE_vec <- sapply((v):(length(data)-v-p),
+                    function(t) outofsample_fit(X,Y,h,v,t,p))
+
+  MSE     <- mean(MSE_vec)
+
+  return(MSE)
+
+}
--- PS6/tsmodelselect/R/main.R
+++ PS6/tsmodelselect/R/main.R
@@ -14,7 +14,7 @@
 #' @examples
 test_criteria_AR <- function(Phi0,Sigma0,n,Nsim,m,p0,maxLags) {
 
-  results              <- array(NA,dim=c(Nsim,maxLags,2))
+  results              <- array(NA,dim=c(Nsim,maxLags,3))
   error_prediction     <- matrix(NA,Nsim,maxLags)
 
   set.seed(12345)
@@ -44,8 +44,7 @@ test_criteria_AR <- function(Phi0,Sigma0,n,Nsim,m,p0,maxLags) {
   MSE            <- colMeans(error_prediction^2)
 
   return(list(table_data = table_data,
-              MSE = MSE,
-              error_prediction = error_prediction))
+              MSE = MSE))
 
 }
 
@@ -83,6 +82,56 @@ test_criteria_MA <- function(Theta0,Sigma0,n,Nsim,m,q0,maxLags) {
 
       results[s,p,1:2] <- unlist(criteria_func(Sigma=Sigma,n=n,p=p))
       # results[s,p,3] <- cross_validation_method(data,p)
+
+      error_prediction[s,p] <- as.numeric(data[n+1]-
+                                          predict(model_AR,n.ahead=1)$pred)
+
+    }
+  }
+
+  table_data     <- select_model(results,maxLags)
+  MSE            <- colMeans(error_prediction^2)
+
+  return(list(table_data = table_data,
+              MSE = MSE))
+
+}
+
+
+#' Title
+#'
+#' @param Theta0
+#' @param Sigma0
+#' @param n
+#' @param Nsim
+#' @param m
+#' @param q0
+#' @param maxLags
+#'
+#' @return
+#' @export
+#'
+#' @examples
+test_criteria_ARMA <- function(Phi0,Theta0,Sigma0,n,Nsim,m,p0,q0,maxLags) {
+
+  results              <- array(NA,dim=c(Nsim,maxLags,3))
+  error_prediction     <- matrix(NA,Nsim,maxLags)
+
+  for(s in 1:Nsim) {
+
+    print(paste('Simulation:',s,'of',Nsim))
+    data <- sample_dataARMA(Phi=Phi0,Theta=Theta0,Sigma=Sigma0,
+                            p0=p0,q0=q0,m=m,n=n+1)
+
+    for(p in 1:maxLags) {
+
+      model_AR  <- arima(as.data.frame(data[(maxLags-p+1):(n)]),order=c(p,0,0),
+                         include.mean=FALSE)
+
+      Sigma    <- (1/(n-maxLags-p))*t(model_AR$residuals)%*%model_AR$residuals
+
+      results[s,p,1:2] <- unlist(criteria_func(Sigma=Sigma,n=n,p=p))
+      results[s,p,3]   <- cv_AR(data[(maxLags-p+1):(n)],h=25,v=5,p=p)
 
       error_prediction[s,p] <- as.numeric(data[n+1]-
                                           predict(model_AR,n.ahead=1)$pred)
